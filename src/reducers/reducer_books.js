599bc567fb8300e1646dc013823084aa46b350cd
JeongMin Woo <minhan_a@naver.com>
2018-10-17 15:59:27 +0900

    react-redux create and BookList connect

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+  return[
+    {title:'Javascript'},
+    {title:'Harry Potter'},
+    {title:'The Dart Tower'},
+    {title:'Eloquent Ruby'},
+    {title:'Hello World'}
+  ]
+}
