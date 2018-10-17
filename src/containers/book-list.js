07c32d3de29c35ef66924e42e9ad7267c50928eb
JeongMin Woo <minhan_a@naver.com>
2018-10-17 16:35:24 +0900

    action creator

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component{
 
   renderList(){
     return this.props.books.map((book)=>{
       return(
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          onClick={()=> this.props.selectBook(book)} 
+          key={book.title}
+          className="list-group-item">
+          {book.title}
+          </li>
       );
     });
   }
@@ -21,10 +28,13 @@ class BookList extends Component{
 }
 
 function mapStateToProps(state) {
-  
   return{
     books : state.books
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({selectBook:selectBook}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
