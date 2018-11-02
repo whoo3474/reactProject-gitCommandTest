7d21e8521b2ce2fc8f8e54969a895b06b6b4bd13
JeongMin Woo <minhan_a@naver.com>
2018-11-02 14:00:26 +0900

    doit Redirect

--- rr4-tut/src/components/Header.js
+++ rr4-tut/src/components/Header.js
@@ -6,8 +6,10 @@ const Header = () => {
   return (
     <div className="header">
       <NavLink to="/" className="item" activeClassName="active" exact>홈</NavLink>
-      <NavLink to="/about/jeongmin" className="item">소개</NavLink>
-      <NavLink to="/posts" className="item">포스트</NavLink>
+      <NavLink to="/about/jeongmin" className="item" activeClassName="active">소개</NavLink>
+      <NavLink to="/posts" className="item" activeClassName="active">포스트</NavLink>
+      <NavLink to="/login" className="item" activeClassName="active">로그인</NavLink>
+      <NavLink to="/me" className="item" activeClassName="active">마이페이지</NavLink>
     </div>
 
   );
