44768c458a8006c08f4f3ffb040e6fb2ba15ada7
JeongMin Woo <minhan_a@naver.com>
2018-11-02 13:55:43 +0900

    doit navlink

--- rr4-tut/src/components/Header.js
+++ rr4-tut/src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
     <div className="header">
-      <Link to="/" className="item">홈</Link>
-      <Link to="/about/jeongmin" className="item">소개</Link>
-      <Link to="/posts" className="item">포스트</Link>
+      <NavLink to="/" className="item" activeClassName="active" exact>홈</NavLink>
+      <NavLink to="/about/jeongmin" className="item">소개</NavLink>
+      <NavLink to="/posts" className="item">포스트</NavLink>
     </div>
 
   );
