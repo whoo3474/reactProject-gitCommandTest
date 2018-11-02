ebee0f524491c144270460692bfa6330f6e9bf9f
JeongMin Woo <minhan_a@naver.com>
2018-11-02 13:57:04 +0900

    create mypage & login

--- rr4-tut/src/App.js
+++ rr4-tut/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
 import Posts from './routes/Posts';
+import MyPage from './routes/MyPage';
+import Login from './routes/Login';
 
 import Header from './components/Header'
 
@@ -14,6 +16,8 @@ const App = () => {
         <Route exact path="/" component={Home}/>
         <Route path="/about/:username" component={About}/>
         <Route path="/posts" component={Posts}/>
+        <Route path="/me" component={MyPage}/>
+        <Route path="/login" component={Login}/>
       </div>
     </Router>
   );
