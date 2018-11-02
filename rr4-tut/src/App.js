4395adbeecde2f3f653752d7c3d027411dde5b9d
JeongMin Woo <minhan_a@naver.com>
2018-11-02 13:44:19 +0900

    create header.js

--- rr4-tut/src/App.js
+++ rr4-tut/src/App.js
@@ -3,11 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
 
+import Header from './components/Header'
+
 const App = () => {
   return (
     <Router>
       <div>
-        <Route path="/" component={Home}/>
+        <Header/>
+        <Route exact path="/" component={Home}/>
         <Route path="/about" component={About}/>
       </div>
     </Router>
