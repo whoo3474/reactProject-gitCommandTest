df97f3ba23587a6e431f8c41535d926d3c585370
JeongMin Woo <minhan_a@naver.com>
2018-11-02 13:38:04 +0900

    create about.js

--- rr4-tut/src/App.js
+++ rr4-tut/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
 
 const App = () => {
   return (
     <Router>
       <div>
-        hello
+        <Route path="/" component={Home}/>
+        <Route path="/about" component={About}/>
       </div>
     </Router>
   );
