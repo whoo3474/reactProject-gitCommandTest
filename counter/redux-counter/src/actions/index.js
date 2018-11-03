cb624a483adbdd993ff08f52316f868c5254620a
JeongMin Woo <minhan_a@naver.com>
2018-11-03 22:37:11 +0900

    create reducer

--- counter/redux-counter/src/actions/index.js
+++ counter/redux-counter/src/actions/index.js
@@ -1,14 +1,14 @@
 import * as types from './ActionTypes';
 
-export const increment = () => {
+export const increment = () => ({
   type: types.INCREMENT
-};
+});
 
-export const decrement = () => {
+export const decrement = () => ({
   type: types.DECREMENT
-};
+});
 
-export const setColor = (color) => {
+export const setColor = (color) => ({
   type: types.SET_COLOR,
   color
-};
+});
