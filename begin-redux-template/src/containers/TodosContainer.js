7aa7cf9c8e20b3ffdf0ba031f07f6975c7864518
JeongMin Woo <minhan_a@naver.com>
2018-11-08 21:15:29 +0900

    base-redux : create TodosContainer.js

--- begin-redux-template/src/containers/TodosContainer.js
+++ begin-redux-template/src/containers/TodosContainer.js
@@ -1,1 +1,13 @@
 // 리덕스와 연동된 컨테이너 컴포넌트 작성
+import React, {Component} from 'react';
+import Todos from 'components/Todos';
+
+class TodosContainer extends Component {
+  render() {
+    return (
+      <Todos />
+    );
+  }
+}
+
+export default TodosContainer;
