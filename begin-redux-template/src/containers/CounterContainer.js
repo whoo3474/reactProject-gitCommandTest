68eb59490c7df4ddc7e4fc76b4bcdf1766666b60
JeongMin Woo <minhan_a@naver.com>
2018-11-08 19:11:33 +0900

    base-redux : create CounterContainer.js

--- begin-redux-template/src/containers/CounterContainer.js
+++ begin-redux-template/src/containers/CounterContainer.js
@@ -1,1 +1,13 @@
 // 리덕스와 연동된 컨테이너 컴포넌트 작성
+import React, { Component } from 'react';
+import Counter from 'components/Counter';
+
+class CounterContainer extends Component {
+  render() {
+    return (
+      <Counter />
+    );
+  }
+}
+
+export default CounterContainer;
