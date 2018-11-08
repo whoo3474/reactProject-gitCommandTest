cfc98b8db61a13bba55e7a3911505cf6deb81837
JeongMin Woo <minhan_a@naver.com>
2018-11-09 07:03:47 +0900

    redux-starter-kit : create redux-logger myself

--- redux-starter-kit-master/src/store.js
+++ redux-starter-kit-master/src/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
+import loggerMiddleware from './lib/loggerMiddleware';
 
-const store = createStore(modules)
+const store = createStore(modules, applyMiddleware(loggerMiddleware))
 
 export default store;
