80b464f34c3e2dadb72aa7bd7b7acdbc4bdb9a34
JeongMin Woo <minhan_a@naver.com>
2018-11-08 18:35:43 +0900

    base-redux : create modules/counter.js of a duck type

--- begin-redux-template/src/store/modules/counter.js
+++ begin-redux-template/src/store/modules/counter.js
@@ -1,1 +1,22 @@
 // 카운터 관련 상태 로직
+
+const INCREMENT = 'counter/INCREMENT';
+const DECREMENT = 'counter/DECREMENT';
+
+export const increment = () => ({ type: INCREMENT });
+export const decrement = () => ({ type: DECREMENT });
+
+const initialState = {
+  number : 0
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case INCREMENT:
+      return { number: state.number + 1};
+    case DECREMENT:
+      return { number: state.number - 1};
+    default:
+      return state;
+  }
+}
