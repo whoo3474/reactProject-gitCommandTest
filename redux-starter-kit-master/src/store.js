48298688766ef4b10af13024ed79ea4646fc76d4
JeongMin Woo <minhan_a@naver.com>
2018-11-09 08:57:56 +0900

    redix-starter-kit : add redux-promise-middleware

--- redux-starter-kit-master/src/store.js
+++ redux-starter-kit-master/src/store.js
@@ -2,9 +2,12 @@ import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
 import {createLogger} from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
 
 const logger = createLogger();
-
-const store = createStore(modules, applyMiddleware(logger,ReduxThunk))
+const customizedPromiseMiddleware = promiseMiddleware({
+  promiseTypeSuffixes: ['LOADING', 'SUCCESS', 'FAILURE']
+});
+const store = createStore(modules, applyMiddleware(logger,ReduxThunk,customizedPromiseMiddleware));
 
 export default store;
