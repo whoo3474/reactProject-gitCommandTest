8265329aa9144190222db0bdff5cf9f157f75aaf
JeongMin Woo <minhan_a@naver.com>
2018-11-08 19:30:28 +0900

    base-redux : connect redux

--- begin-redux-template/src/containers/CounterContainer.js
+++ begin-redux-template/src/containers/CounterContainer.js
@@ -1,13 +1,38 @@
 // 리덕스와 연동된 컨테이너 컴포넌트 작성
 import React, { Component } from 'react';
 import Counter from 'components/Counter';
+import { connect } from 'react-redux';
+import * as counterActions from 'store/modules/counter';
 
 class CounterContainer extends Component {
+  handleIncrement = () => {
+    this.props.increment();
+  }
+  handleDecrement = () => {
+    this.props.decrement();
+  }
+
   render() {
+    const { handleDecrement, handleIncrement } = this;
+    const { number } = this.props;
+
     return (
-      <Counter />
+      <Counter 
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+        number = {number}
+      />
     );
   }
 }
 
-export default CounterContainer;
+const mapStateToProps = (state) => ({
+  number: state.counter.number
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  increment: () => dispatch(counterActions.increment()),
+  decrement: () => dispatch(counterActions.decrement())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
